b0171d0c4e53870fbe76d10f9d51c863a3221b45
aurelsoleil <aurelien.soleil@iut-rodez.fr>
2026-01-20 16:12:20 +0100

    add few components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+"use client"
+import { Heading } from '@/components/ui/Heading/Heading';
+import { Box } from '@/components/ui/Box/Box';
+import { Container } from '@/components/ui/Container/Container';
+import { Stack } from '@/components/ui/Stack/Stack';
+import { Text } from '@/components/ui/Text/Text';
+
 export default function Home() {
   return (
-    <main>
-      <h1>Ma Petite Compagnie</h1>
-      <p>Application de gestion pour compagnie de théâtre</p>
-    </main>
+    <Container maxW="container.md" py={20}>
+      <Stack gap={4} textAlign="center">
+        <Heading as="h1" size="2xl">Ma Petite Compagnie</Heading>
+        <Text fontSize="xl" color="gray.600">Application de gestion pour compagnie de théâtre</Text>
+      </Stack>
+    </Container>
   );
 }
