8d766ced481dfba220fc1ac78b856f03114f2f17
enzo2900 <133020135+enzo2900@users.noreply.github.com>
2026-01-20 11:40:23 +0100

    Ajout de la fonction read update et delete + Ajout d'un test

--- src/app/communication/util/contact.ts
+++ src/app/communication/util/contact.ts
@@ -1,11 +1,21 @@
-import { PrismaClient } from "@prisma/client";
-
+import { Contact, PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 export async function createContact(nom: string, prenom: string) {
-  const prisma = new PrismaClient();
   const newContact = await prisma.contact.create({
     data: {
       nom: nom,
       prenom: prenom,
     },
   });
+}
+export async function getMany() {
+  return await prisma.contact.findMany();
+}
+
+export async function update(contact: Contact, newContact: Contact) {
+  return prisma.contact.update({ where: { ...contact }, data: { ...newContact } });
+}
+
+export async function deleteContact(contact: Contact) {
+  return prisma.contact.delete({ where: { ...contact } });
 }
