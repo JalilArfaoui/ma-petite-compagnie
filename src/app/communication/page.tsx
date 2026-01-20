61e57392c4896fc670ea189dd8cb3108b3aa456e
enzo2900 <133020135+enzo2900@users.noreply.github.com>
2026-01-20 11:40:23 +0100

    Ajout de la fonction read update et delete + Ajout d'un test

--- src/app/communication/page.tsx
+++ src/app/communication/page.tsx
@@ -1,4 +1,8 @@
-export default function Contact() {
+import { createContact, getMany } from "./util/contact";
+
+export default async function Contact() {
+  createContact("Mec", "Prenom");
+  await getMany();
   return (
     <main>
       <h1>Page de contact</h1>
