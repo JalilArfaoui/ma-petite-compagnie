a4a1f77b68e8dc273c192c4a17473a3d7d19ecd4
enzo2900 <133020135+enzo2900@users.noreply.github.com>
2026-01-26 16:54:09 +0100

    Ajout de tests pour les appels api de contact. Ajout d'une dépendance dans package.json (next/env) pour les variables d'environnement dans un test runner

--- src/app/communication/api/contact.ts
+++ src/app/communication/api/contact.ts
@@ -1,17 +1,25 @@
 import { Contact, PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
+import "../../../../envConfig.ts";
+console.log("env database is " + process.env.DATABASE_URL);
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
 const prisma = new PrismaClient({ adapter });
 
 export async function createContact(nom: string, prenom: string) {
+  console.log(process.env.DATABASE_URL);
   const newContact = await prisma.contact.create({
     data: {
       nom: nom,
       prenom: prenom,
     },
   });
+  return newContact;
+}
+
+export async function trouverParNom(nom: string) {
+  return await prisma.contact.findFirst({ where: { nom: nom } });
 }
 export async function getMany() {
   return await prisma.contact.findMany();
@@ -21,6 +29,6 @@ export async function update(contact: Contact, newContact: Contact) {
   return prisma.contact.update({ where: { ...contact }, data: { ...newContact } });
 }
 
-export async function deleteContact(contact: Contact) {
-  return prisma.contact.delete({ where: { ...contact } });
+export async function supprimerParId(contactId: number) {
+  return prisma.contact.delete({ where: { id: contactId } });
 }
