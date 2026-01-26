d649467afe8479c306b47149f9762f34ba62aaf8
enzo2900 <133020135+enzo2900@users.noreply.github.com>
2026-01-26 19:59:16 +0100

    Ajout d'un composant pour créer ou modifier un contact

---
+++ src/app/communication/components/ContactModification.tsx
@@ -0,0 +1,68 @@
+import { Contact } from "@prisma/client";
+import { useState } from "react";
+
+function getOrReplace<T>(champ: T | null, replace: T) {
+  if (!champ) {
+    return replace;
+  } else {
+    return champ;
+  }
+}
+export function ContactModification({
+  onSubmitted,
+  contactDonnee,
+}: {
+  onSubmitted: (donneeFormulaire: FormData) => void;
+  contactDonnee: Contact;
+}) {
+  const [nom, setNom] = useState(getOrReplace(contactDonnee.nom, ""));
+  const [prenom, setPrenom] = useState(getOrReplace(contactDonnee.prenom, ""));
+  const [email, setEmail] = useState(getOrReplace(contactDonnee.email, ""));
+  const [tel, setTel] = useState(getOrReplace(contactDonnee.tel, ""));
+  const [role, setRole] = useState(getOrReplace(contactDonnee.role, "USER"));
+  return (
+    <>
+      <div>
+        <form action={onSubmitted}>
+          <label>
+            Nom : <input name="nom" onChange={(e) => setNom(e.target.value)} value={nom} />
+          </label>
+          <label>
+            Prenom :{" "}
+            <input name="prenom" onChange={(e) => setPrenom(e.target.value)} value={prenom} />
+          </label>
+          <label>
+            Email : <input name="email" onChange={(e) => setEmail(e.target.value)} value={email} />
+          </label>
+          <label>
+            Téléphone : <input name="tel" onChange={(e) => setTel(e.target.value)} value={tel} />
+          </label>
+          <p>
+            Rôles
+            <label>
+              Utilisateur :{" "}
+              <input
+                type="radio"
+                name="role"
+                value="USER"
+                onChange={(e) => setRole("USER")}
+                checked={role === "USER"}
+              />
+            </label>
+            <label>
+              Partenaire :
+              <input
+                type="radio"
+                name="role"
+                value="PARTENAIRE"
+                onChange={(e) => setRole("PARTENAIRE")}
+                checked={role === "PARTENAIRE"}
+              />
+            </label>
+          </p>
+          <button type="submit">Confirmer</button>
+        </form>
+      </div>
+    </>
+  );
+}
