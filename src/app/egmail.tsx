0d7f13a2165e4d0d4fc9bbe2b0439e22876003c2
Hessenette <ciranyuksel@gmail.com>
2026-01-27 10:57:32 +0100

    egmail  le formulmaire

--- src/app/egmail.tsx
+++ src/app/egmail.tsx
@@ -1,10 +1,63 @@
-import { useState } from "react";
+import { strict } from "assert";
+import { ChangeEvent, FormEvent, useState } from "react";
 
-function EnvoieCouriel() {
-  const [form, setForm] = useState({
-    à: "",
+interface FormulaireEmail {
+  destinataire : string;
+  sujet: string ;
+  message: string;
+}
+
+ export default function EnvoieCouriel() {
+  const [formulaire, setFormulaire] = useState<FormulaireEmail>({
+    destinataire: "",
     sujet: "",
     message: "",
   });
-  
+
+  const gererChangement = (
+    e: ChangeEvent<HTMLInputElement| HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormulaire({
+      ...formulaire,
+      [name]: value,
+    });
+  };
+
+  const gererEnvoi = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(formulaire);
+  };
+
+return (
+    <form onSubmit={gererEnvoi}>
+      <input
+        type="email"
+        name="destinataire"
+        placeholder="Destinataire"
+        value={formulaire.destinataire}
+        onChange={gererChangement}
+        required
+      />
+
+      <input
+        type="text"
+        name="sujet"
+        placeholder="Sujet"
+        value={formulaire.sujet}
+        onChange={gererChangement}
+        required
+      />
+
+      <textarea
+        name="message"
+        placeholder="Message"
+        value={formulaire.message}
+        onChange={gererChangement}
+        required
+      />
+
+      <button type="submit">Envoyer</button>
+    </form>
+  );
 }
