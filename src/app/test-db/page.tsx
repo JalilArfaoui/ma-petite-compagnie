b9a8201ce76bfbcb28c4b843a6592df0059facf4
Sylvell <adrien@alteirac.com>
2026-01-27 10:45:49 +0100

    prisma create spectacle

--- src/app/test-db/page.tsx
+++ src/app/test-db/page.tsx
@@ -6,6 +6,16 @@ export default async function TestDbPage() {
   const result = await prisma.$queryRaw<[{ now: Date }]>`SELECT NOW() as now`;
   const dbTime = result[0].now;
 
+  await prisma.spectacle.create({
+  data: {
+    id: 1,
+    description: 'test_description',
+    type: "test_type",
+    statut: "test_statut",
+    troupe: "test_troupe",
+  },
+  });
+
   return (
     <div>
       <h1>Test connexion Prisma</h1>
