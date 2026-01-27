8190c8f00319e4c6b3a837125021107f9eea5f13
Mateofsr <90698344+Mateofsr@users.noreply.github.com>
2026-01-27 10:59:33 +0100

    chore:Créer un lieu côté API

---
+++ src/app/api/lieux/lieux.ts
@@ -0,0 +1,46 @@
+import { NextResponse, NextRequest } from "next/server"
+import { prisma } from "@/lib/prisma"
+
+type CreateLieuBody = {
+    libelle:string,
+    adresse:string,
+    ville:string,
+    numero_salle:string|null,
+    idCompagnie:number
+}
+
+export async function POST(req:NextRequest) {
+    try {
+        const body:CreateLieuBody = await req.json();
+        const {
+            libelle,
+            adresse,
+            ville,
+            numero_salle,
+            idCompagnie
+        } = body;
+        if (!libelle || !adresse || ville || idCompagnie) {
+            return NextResponse.json(
+                { message: "Des informations sont manquantes." },
+                { status: 400 }
+            )
+        }
+
+        const lieu = await prisma.lieu.create({
+          data: {
+            libelle,
+            adresse,
+            ville,
+            numero_salle,
+            idCompagnie,
+          },
+        });
+        return {lieu};
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json(
+            { message: "Erreur serveur:" + error },
+            { status: 500 }
+        )
+    }
+}
