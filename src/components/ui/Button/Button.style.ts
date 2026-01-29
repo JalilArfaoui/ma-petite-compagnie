26cbd273add79409d84f2473b7a43fddd097378e
Edemit <shuldeshovwork013@gmail.com>
2026-01-29 07:31:37 +0100

    Removing border from button

---
+++ src/components/ui/Button/Button.style.ts
@@ -0,0 +1,23 @@
+import { Button, ButtonProps } from "@chakra-ui/react";
+import styled, { css } from "styled-components";
+
+export const StyledButton = styled(Button)<ButtonProps>`
+  width: 200px;
+  height: 40px;
+  color: #ffffff;
+  background-color: #d00039;
+  border-radius: 20px;
+  font-family: Georgia;
+  font-style: italic;
+  font-weight: bold;
+
+  ${(props) =>
+    props.variant === "outline" &&
+    css`
+      color: #000000;
+      background-color: #ffffff;
+      border: 1px solid #e1e8f1;
+      font-style: normal;
+      font-weight: normal;
+    `}
+`;
