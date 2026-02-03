61690f1ab88e1a6df54dee49d04517ddf6c9c48c
florianstudent <tempocounti@outlook.fr>
2026-02-03 11:45:16 +0100

    test affichage page avec composants graphiques

---
+++ src/app/finances/section-export-fichiers/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+import { Heading, Container, Stack, Text, Box, Button, Card } from "@/components/ui";
+import { FaHome } from "react-icons/fa";
+
+export default function Home()
+{
+    <Box display={"flex"} style={{ marginTop: "80px", gap: "4em", flexWrap: "wrap", justifyContent: "center" }}></Box>
+}
