9d28448f2418988ca04d688103cd5dde22efa4ce
enzo2900 <133020135+enzo2900@users.noreply.github.com>
2026-02-04 15:27:26 +0100

    Correction des tests

--- src/test/contact.test.tsx
+++ src/test/contact.test.tsx
@@ -1,11 +1,11 @@
-import { ContactInformation } from "../app/communication/api/contact/contact";
+import { creerUnContact, ContactInformation } from "../app/communication/api/contact/contact";
 import { describe, it, expect } from "vitest";
 
 function creerObjetContactAvecNom(nom: string): ContactInformation {
-  return { nom: nom, prenom: "User", email: null, tel: null, role: null };
+  return { nom: nom, prenom: "User", email: "email@email.com", tel: "0011001100", role: null };
 }
 
 it("Créer un mauvais contact", async () => {
-  const result = await createContact(creerObjetContactAvecNom(""));
-  expect(result.success).toBe(false);
+  const result = await creerUnContact(creerObjetContactAvecNom(""));
+  expect(result.succes).toBe(false);
 });
