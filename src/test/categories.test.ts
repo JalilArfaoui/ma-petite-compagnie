d87453ad82b338509164316ea904fab709993a56
Mateofsr <mateo.faussurier@iut-rodez.fr>
2026-02-10 10:22:24 +0100

    feat:Test de l'ajout d'une catégorie

---
+++ src/test/categories.test.ts
@@ -0,0 +1,34 @@
+import { describe, it, expect, vi } from "vitest";
+import {prisma} from "@/lib/prisma";
+import {NextRequest} from "next/server";
+import {POST} from "@/app/api/categories/route";
+vi.mock("@/lib/prisma", () => ({
+    prisma:{
+        categorie:{
+            create: vi.fn()
+        }
+    }
+}))
+
+describe("POST /api/categories", () => {
+    it("crée une catégorie et retourne 201", async () => {
+        const mockedCreate = vi.mocked(prisma.categorie.create);
+        const nom = "Répétition";
+        mockedCreate.mockResolvedValue({
+            id:1,
+            nom:nom,
+            idCompagnie:1
+        });
+        const req = new NextRequest("http://localhost/api/categories", {
+            method: "POST",
+            body:JSON.stringify({
+                nom:nom,
+                idCompagnie:1
+            })
+        });
+        const res = await POST(req);
+        expect(res.status).toBe(201);
+        const json = await res.json();
+        expect(json.nom).toBe(nom);
+    })
+})
