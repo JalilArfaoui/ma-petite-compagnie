2da5b3243439a948fcc58072801e1db88a78bc8f
Mateofsr <mateo.faussurier@iut-rodez.fr>
2026-02-10 09:57:01 +0100

    chore:Intégration de la liste des lieux

--- src/app/composants/lieux/CreateLieuForm.tsx
+++ src/app/composants/lieux/CreateLieuForm.tsx
@@ -1,26 +1,27 @@
 "use client";
 import type { Lieu } from "@/types/lieu";
 import { useState } from "react";
-import { Field } from "@chakra-ui/react";
-import {Button, Input} from "@/components/ui";
+import {Field, GridItem} from "@chakra-ui/react";
+import {Button, Input, SimpleGrid} from "@/components/ui";
 
 type Props = {
   onSuccess: (lieu: Lieu) => void;
   onCancel: () => void;
+  idCompagnie: number;
 };
-export function CreateLieuForm({ onSuccess, onCancel }: Props) {
+export function CreateLieuForm({ onSuccess, onCancel, idCompagnie }: Props) {
   const [libelle, setLibelle] = useState("");
   const [adresse, setAdresse] = useState("");
   const [ville, setVille] = useState("");
   const [numeroSalle, setNumeroSalle] = useState("");
 
-  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+  async function handleSubmitLieu(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
 
     const res = await fetch("/api/lieux", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ libelle, adresse, ville, numeroSalle }),
+      body: JSON.stringify({ libelle, adresse, ville, numeroSalle, idCompagnie }),
     });
 
     if (!res.ok) {
@@ -34,7 +35,7 @@ export function CreateLieuForm({ onSuccess, onCancel }: Props) {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmitLieu}>
       <div>
         <Field.Root required>
           <Field.Label>
@@ -79,20 +80,27 @@ export function CreateLieuForm({ onSuccess, onCancel }: Props) {
         </Field.Root>
       </div>
       <div>
-        <Field.Root required>
+        <Field.Root>
           <Field.Label>
             N° de la salle <Field.RequiredIndicator />
           </Field.Label>
           <Input type="text" value={numeroSalle} onChange={(e) => setNumeroSalle(e.target.value)} />
         </Field.Root>
       </div>
-      <Button type="submit" disabled={!libelle || !adresse || !ville}>
-        Créer
-      </Button>
-
-      <Button type="button" onClick={onCancel}>
-        Annuler
-      </Button>
+      <SimpleGrid columns={{ base: 4, md: 5 }} gap={{ base: "0px", md: "0px" }}>
+        <GridItem colSpan={{ base: 1, md: 1 }}></GridItem>
+        <GridItem colSpan={{ base: 1, md: 1 }}>
+          <Button type="button" onClick={onCancel}>
+            Annuler
+          </Button>
+        </GridItem>
+        <GridItem colSpan={{ base: 1, md: 1}}></GridItem>
+        <GridItem colSpan={{ base: 1, md: 1 }}>
+          <Button type="submit" disabled={!libelle || !adresse || !ville}>
+            Créer
+          </Button>
+        </GridItem>
+      </SimpleGrid>
     </form>
   );
 }
