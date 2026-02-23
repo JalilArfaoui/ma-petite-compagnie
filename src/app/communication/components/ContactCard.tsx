7c9165c8c564b021b1a3c3cd499697a0c660324f
enzo2900 <133020135+enzo2900@users.noreply.github.com>
2026-02-23 22:31:11 +0100

    Modification des styles pour qu'il utilise tailwind + mise a jour du projet

--- src/app/communication/components/ContactCard.tsx
+++ src/app/communication/components/ContactCard.tsx
@@ -7,21 +7,21 @@ export function ContactCard({ contact }: { contact: Contact }) {
         {contact.nom} {contact.prenom}
       </Heading>
 
-      <Card.Body padding={"3"}>
+      <Card.Body className="p-3">
         <Stack gap={4}>
           <Box>
-            <Text fontWeight="bold">Email : </Text>
+            <Text className=" font-bold">Email : </Text>
             <Text>{contact.email || "Non renseigné"}</Text>
           </Box>
           <Box>
-            <Text fontWeight="bold">Téléphone :</Text>
+            <Text className=" font-bold">Téléphone :</Text>
             <Text> {contact.tel || "Non renseigné"}</Text>
           </Box>
           <Box>
-            <Text fontWeight="bold">Rôle :</Text> <Text>{contact.role || "Non défini"}</Text>
+            <Text className=" font-bold">Rôle :</Text> <Text>{contact.role || "Non défini"}</Text>
           </Box>
           <Box>
-            <Text fontWeight="bold">Créé le</Text>
+            <Text className=" font-bold">Créé le</Text>
 
             <Text>
               {contact.date_creation
