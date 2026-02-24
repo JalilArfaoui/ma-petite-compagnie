fbc3ee5e2955b355e348300324774642433f16a5
Yuksel <ciranyuksel@gmail.com>
2026-02-24 11:25:03 +0100

    format
    Npm run format

--- src/app/email/Action/EnvoieEmail.tsx
+++ src/app/email/Action/EnvoieEmail.tsx
@@ -1,21 +1,21 @@
-import { BrevoClient } from "@getbrevo/brevo" ;
+import { BrevoClient } from "@getbrevo/brevo";
 
-export const EnvoieEmail = async ( email: string , subject: string , content: string ) => {
-    const client = new BrevoClient ( {apiKey: process.env.BREVO_API_KEY || ""} ) ;
-    try {
-        const response = await client.transactionalEmails.sendTransacEmail ( {
-            sender : {
-                name : "Ma Petite Compagnie" ,
-                email : "contact@yourcompany.com"
-            } ,
-            to : [{email}] ,
-            subject : subject ,
-            htmlContent : content
-        } ) ;
-        return { success: true, response } ;
-    } catch ( error: any ) {
-        console . error ( "Error avec Brevo:" , error ) ;
-        return { success: false, error } ;
-        throw error ;   
-    }
-}
+export const EnvoieEmail = async (email: string, subject: string, content: string) => {
+  const client = new BrevoClient({ apiKey: process.env.BREVO_API_KEY || "" });
+  try {
+    const response = await client.transactionalEmails.sendTransacEmail({
+      sender: {
+        name: "Ma Petite Compagnie",
+        email: "contact@yourcompany.com",
+      },
+      to: [{ email }],
+      subject: subject,
+      htmlContent: content,
+    });
+    return { success: true, response };
+  } catch (error: any) {
+    console.error("Error avec Brevo:", error);
+    return { success: false, error };
+    throw error;
+  }
+};
