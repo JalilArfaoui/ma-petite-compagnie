f119ca01a79c6e0f7a1f4935b42a372234878591
enzo2900 <133020135+enzo2900@users.noreply.github.com>
2026-03-23 13:22:49 +0100

    Modification de l'affichage en ligne

--- src/app/communication/components/ContactCard.tsx
+++ src/app/communication/components/ContactCard.tsx
@@ -8,33 +8,40 @@ export function ContactCard({
   onSelect: (contact: Contact) => void;
 }) {
   return (
-    <Card onClick={() => onSelect(contact)}>
+    <Card
+      onClick={() => onSelect(contact)}
+      className="bg-gray-200 hover:bg-gray-50 focus:outline-2 focus:outline-offset-2 focus:outline-violet-500 active:bg-orange-300"
+    >
       <Heading as={"h4"}>
         {contact.nom} {contact.prenom}
       </Heading>
 
-      <Card.Body className="p-3">
-        <Stack gap={4}>
-          <Box>
-            <Text className=" font-bold">Email : </Text>
-            <Text>{contact.email || "Non renseigné"}</Text>
-          </Box>
-          <Box>
-            <Text className=" font-bold">Téléphone :</Text>
-            <Text> {contact.tel || "Non renseigné"}</Text>
-          </Box>
-          <Box>
-            <Text className=" font-bold">Rôle :</Text> <Text>{contact.role || "Non défini"}</Text>
-          </Box>
-          <Box>
-            <Text className=" font-bold">Créé le</Text>
+      <Card.Body className=" pl-2">
+        <Stack gap={1}>
+          <Stack direction="column" gap={1}>
+            <Box>
+              <Text className=" font-bold">Email : </Text>
+              <Text>{contact.email || "Non renseigné"}</Text>
+            </Box>
+            <Box>
+              <Text className=" font-bold">Téléphone :</Text>
+              <Text> {contact.tel || "Non renseigné"}</Text>
+            </Box>
+            <Box>
+              <Text className=" font-bold">Rôle :</Text> <Text>{contact.role || "Non défini"}</Text>
+            </Box>
+          </Stack>
+          <Stack direction="row-reverse" justify="end">
+            <Box>
+              <Text className=" font-bold">Créé le</Text>
 
-            <Text>
-              {contact.date_creation
-                ? new Date(contact.date_creation).toLocaleDateString()
-                : "Non défini"}
-            </Text>
-          </Box>
+              <Text>
+                {contact.date_creation
+                  ? new Date(contact.date_creation).toLocaleDateString()
+                  : "Non défini"}
+              </Text>
+            </Box>
+          </Stack>
         </Stack>
       </Card.Body>
     </Card>
