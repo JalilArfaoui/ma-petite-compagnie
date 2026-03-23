aa9f542073b9e6ef8a09d521f0dc9c5701b40f53
enzo2900 <133020135+enzo2900@users.noreply.github.com>
2026-03-23 16:18:40 +0100

    Ajout d'un composant pour faire de l'importation de fichier CSV

---
+++ src/app/communication/components/CSVContactImport.tsx
@@ -0,0 +1,143 @@
+"use client";
+import { Box, Button, Drawer, Input, List, Modal, Select, Stack, Text } from "@/components/ui";
+import { useRef, useState } from "react";
+
+export function CSVContactImport({
+  requiredAttributes,
+  optionnalAttributes,
+  onCSVRead,
+}: {
+  requiredAttributes: string[];
+  optionnalAttributes: string[];
+  onCSVRead: (donnees: Record<string, string>[]) => void;
+}) {
+  const attributes = [...requiredAttributes, ...optionnalAttributes];
+
+  console.log(attributes);
+
+  const modal = useRef<HTMLButtonElement>(null);
+  const inputFile = useRef<HTMLInputElement>(null);
+  const [champs, setChamps] = useState<string[]>(requiredAttributes ?? []);
+  const handleFile = async (input: HTMLInputElement) => {
+    const file = input.files?.[0];
+    if (!file) return;
+
+    const text = await file.text();
+
+    const rows = text.split("\n").map((row) => row.split(","));
+    console.log(rows);
+    const datas: Record<string, string>[] = [];
+    rows.forEach((row, i) => {
+      if (i === 0) return; // skip header si besoin
+
+      const obj: Record<string, string> = {};
+
+      champs.forEach((champ, index) => {
+        obj[champ] = row[index] || ""; // associe le champ à la valeur de la colonne
+      });
+
+      datas.push(obj);
+    });
+    console.log(datas);
+    onCSVRead(datas);
+  };
+  function changeChamps(index: number, newType: string) {
+    setChamps((prev) =>
+      prev.map((champ, i) => {
+        const result = i === index ? newType : champ;
+        return result;
+      })
+    );
+  }
+  function champsDoublon() {
+    return champs.find((champ, i) => {
+      return champs.find((champ2, i2) => i !== i2 && champ === champ2);
+    });
+  }
+  function champsContientRequired() {
+    return requiredAttributes.every((attribute) => champs.includes(attribute));
+  }
+  function addChamp() {
+    setChamps((prev) => [...prev, ""]);
+  }
+  function supprimerColonne(i: number) {
+    setChamps((prev) => {
+      return prev.filter((champs, inedx) => i !== inedx);
+    });
+  }
+  function confirmation() {
+    console.log("Importation du csv");
+    console.log(attributes);
+    if (inputFile.current?.files) {
+      console.log(champs);
+      if (champsDoublon()) {
+        // TODO erreur
+        return;
+      }
+      if (!champsContientRequired()) {
+        //TODO erreur
+        return;
+      }
+      handleFile(inputFile.current);
+      modal.current?.click();
+    }
+  }
+  return (
+    <Modal>
+      <Modal.Trigger asChild>
+        <Button variant="outline">Import contacts CSV</Button>
+      </Modal.Trigger>
+      <Modal.Content size="md">
+        <Modal.Header>
+          <Modal.Title>Import contacts par CSV</Modal.Title>
+          <Modal.Description>Importez vos contacts</Modal.Description>
+        </Modal.Header>
+        <Modal.Body>
+          <Stack className="gap-4">
+            <Input ref={inputFile} type="file" accept=".csv"></Input>
+            <Text>Colonnes : </Text>
+            {champs.map((attributeChamp, i) => {
+              return (
+                <Stack key={i} direction="row">
+                  {" "}
+                  <Box>(Colonne numéro : {i})</Box>
+                  <select
+                    defaultValue={attributeChamp}
+                    onChange={(e) => changeChamps(i, e.target.value)}
+                  >
+                    {attributes.map((attribute, indexAttribute) => {
+                      return (
+                        <option
+                          key={indexAttribute}
+                          className={
+                            requiredAttributes.includes(attribute) ? "text-red-600" : "text-black"
+                          }
+                        >
+                          {attribute}
+                        </option>
+                      );
+                    })}
+                  </select>
+                  <Button size={"sm"} onClick={() => supprimerColonne(i)}>
+                    Supprimer colonne
+                  </Button>
+                </Stack>
+              );
+            })}
+            <Button onClick={() => addChamp()} size={"sm"}>
+              Ajouter champ
+            </Button>
+          </Stack>
+        </Modal.Body>
+        <Modal.Footer>
+          <Modal.Close asChild ref={modal}>
+            <Button variant="outline">Annuler</Button>
+          </Modal.Close>
+          <Button onClick={() => confirmation()} variant="solid">
+            Importer
+          </Button>
+        </Modal.Footer>
+      </Modal.Content>
+    </Modal>
+  );
+}
--- src/app/communication/page.tsx
+++ src/app/communication/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import { Heading, Link, Box, Button, Stack, SimpleGrid } from "@/components/ui";
-import { listerContacts } from "./api/contact/contact";
+import { creerContact, listerContacts } from "./api/contact/contact";
 import { useEffect, useState } from "react";
-import { Contact } from "@prisma/client";
+import { Contact, Role } from "@prisma/client";
+import { ContactInformation } from "./api/contact/contact";
 import { ContactCard } from "./components/ContactCard";
+import { CSVContactImport } from "./components/CSVContactImport";
 import "../globals.css";
 import { toaster } from "@/components/ui/Toast/toaster";
 export default function ContactPage() {
@@ -19,8 +21,27 @@ export default function ContactPage() {
     }
     loadContact();
   }, []);
+
+  function isContactInformation(obj: unknown): obj is ContactInformation {
+    const o = obj as Record<string, string>;
+    return typeof o.nom === "string" && typeof o.prenom === "string";
+  }
+
+  function csvToContacts(donnees: Record<string, string>[]) {
+    donnees.forEach((element) => {
+      console.log(element);
+      if (isContactInformation(element)) {
+        console.log(element);
+      }
+    });
+  }
   return (
     <Box className=" py-5 flex-col items-center gap-4">
+      <CSVContactImport
+        requiredAttributes={["nom", "prenom"]}
+        optionnalAttributes={["email"]}
+        onCSVRead={csvToContacts}
+      ></CSVContactImport>
       <Stack className="gap-5 items-center">
         <Heading as="h3">Page de contact </Heading>
         <Link href="./communication/contact">
