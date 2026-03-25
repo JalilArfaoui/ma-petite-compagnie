1abdae3563593c4cf8f1b854ebf3018f86c1e9ac
enzo2900 <133020135+enzo2900@users.noreply.github.com>
2026-03-25 18:30:57 +0100

    Affichage des contacts avec un tableau

--- src/app/communication/page.tsx
+++ src/app/communication/page.tsx
@@ -1,99 +1,31 @@
 "use client";
-import { Heading, Link, Box, Button, Stack, SimpleGrid } from "@/components/ui";
-import { listerContacts, supprimerContact } from "./api/contact/contact";
-import { useEffect, useState } from "react";
-import { Contact } from "@prisma/client";
-import { ContactCard } from "./components/ContactCard";
-import "../globals.css";
-import { Toaster, toaster } from "@/components/ui/Toast/toaster";
+import { Heading, Link, Box, Button, Stack, Text } from "@/components/ui";
+
+import { Toaster } from "@/components/ui/Toast/toaster";
+import { ContactTable } from "./components/ContactTable";
 export default function ContactPage() {
-  const [contacts, setContacts] = useState<Contact[]>([]);
-  const [contactsSelectionne, setContactsSelectionne] = useState<Contact[]>([]);
-  const [dernierContactSelect, setDernierContactSelect] = useState<Contact>();
-  useEffect(() => {
-    async function loadContact() {
-      const resultat = await listerContacts(30, 1);
-      if (resultat.succes) {
-        setContacts(resultat.donnee ?? []);
-      } else {
-        toaster.create({ description: resultat.message, type: "error" });
-      }
-    }
-    loadContact();
-  }, []);
-
-  async function supprimerContacts() {
-    await contactsSelectionne.forEach(async (c) => {
-      const resultat = await supprimerContact(c.id);
-      if (!resultat.succes) {
-        toaster.create({ description: resultat.message, type: "error" });
-      }
-    });
-    toaster.create({ description: "Les contacts ont été supprimés", type: "success" });
-    setContacts((prev) => {
-      return prev.filter((c) => !contactsSelectionne.includes(c));
-    });
-    deselectAll();
-  }
-  function updateSelected(contact: Contact) {
-    setDernierContactSelect(contact);
-    setContactsSelectionne((prev) => {
-      if (prev.includes(contact)) {
-        return prev.filter((c) => c !== contact);
-      } else {
-        return [...prev, contact];
-      }
-    });
-  }
-
-  function selectAll() {
-    setContactsSelectionne(() => {
-      return contacts;
-    });
-  }
-
-  function deselectAll() {
-    setContactsSelectionne(() => {
-      return [];
-    });
-  }
   return (
     <Box className=" py-5 flex-col items-center gap-4">
       <Toaster />
-      <Stack className="gap-5 items-center">
-        <Heading as="h3">Page de contact </Heading>
-        <Link href="./communication/contact">
-          <Button className=" scale-0.9">Créer un contact</Button>
-        </Link>
-      </Stack>
-      <Stack direction="row" gap={2}>
-        <Button className=" border-solid " onClick={() => supprimerContacts()}>
-          Supprimer
-        </Button>
-        <Button variant="solid" onClick={() => selectAll()}>
-          Tout sélectionner
-        </Button>
-        <Button variant="solid" onClick={() => deselectAll()}>
-          Tout désélectionner
-        </Button>
+      <Stack
+        className="gap-5 items-center w-full justify-between  mb-10 z-10 bg-white"
+        direction="row"
+      >
+        <Stack className="gap-5 items-center" direction="row">
+          <Heading as="h3">Page de contact </Heading>
+        </Stack>
+        <Stack className="gap-5 items-center w-fit" direction="row" justify="end">
+          <Link href="./communication/contact">
+            <Button size={"sm"} className=" scale-0.9">
+              <Text className="text-white">+ Créer un contact</Text>
+            </Button>
+          </Link>
+        </Stack>
       </Stack>
 
-      <SimpleGrid className="grid grid-cols-1 lg:grid-cols-5" gap={10}>
-        {contacts.map((contact) => {
-          return (
-            <Box className="flex flex-col items-center" key={contact.id}>
-              <Box className={dernierContactSelect === contact ? "orange" : "transparent"}>
-                <ContactCard contact={contact} onSelect={updateSelected} />
-              </Box>
-              {dernierContactSelect === contact && (
-                <Link href={"/communication/" + contact.id}>
-                  <Button>Modifier</Button>
-                </Link>
-              )}
-            </Box>
-          );
-        })}
-      </SimpleGrid>
+      <Box className="md:w-full lg:w-[75%] mx-auto  ">
+        <ContactTable />
+      </Box>
     </Box>
   );
 }
