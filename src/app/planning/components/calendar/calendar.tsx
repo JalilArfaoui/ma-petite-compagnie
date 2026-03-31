8b45c9c79d1d19c89c4e93ebae25443969d3c8cc
matys laurent <matys.laurent@etud.univ-jfc.fr>
2026-03-31 13:09:03 +0200

    modif mineur

--- src/app/planning/components/calendar/calendar.tsx
+++ src/app/planning/components/calendar/calendar.tsx
@@ -205,7 +205,7 @@ const Calendar: React.FC<EventCalendarProps> = ({ events, onEventClick } : Event
     return (
         <div className="event-calendar">
             <div className="calendar-header">
-                <Button variant="solid" onClick={goToPreviousMonth}>
+                <Button variant="solid" onClick={goToPreviousMonth} className='btn-test'>
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
                         <path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z"/>
                     </svg>
@@ -213,21 +213,21 @@ const Calendar: React.FC<EventCalendarProps> = ({ events, onEventClick } : Event
 
                 <div className="header-center">
                     <Heading as="h1" className="month-year">{MONTHS[month - 1]} {year}</Heading>
-                    <Button variant="outline" onClick={goToToday} >
-                        <Heading as="h5"> Aujourd'hui
-                        </Heading>
-                    </Button>
                 </div>
 
                 <div className="buttons-container">
                     <Button variant="outline" onClick={() => setViewType(viewType === 'monthly' ? 'weekly' : 'monthly')} >
                         {viewType === 'monthly' ? 'Semaine' : 'Mois'}
                     </Button>
+                    <Button variant="outline" onClick={goToToday} >
+                        Aujourd'hui
+                    </Button>
                     <Button variant="solid" onClick={goToNextMonth}>
                         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
                             <path d="M504-480 320-664l56-56 240 240-240 240-56-56 184-184Z"/>
                         </svg>
                     </Button>
+                    
                 </div>
             </div>
 
