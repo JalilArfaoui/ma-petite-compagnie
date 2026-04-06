d85ee4bd026eea7d3cb51978dec9b8ed214215b4
Yuksel <ciranyuksel@gmail.com>
2026-04-06 20:19:21 +0200

    Modif
    problème avec mon prisma et test pourquoi ça ne marche pas

--- src/app/communication/action/contactFormAction.tsx
+++ src/app/communication/action/contactFormAction.tsx
@@ -1,15 +1,20 @@
 "use server";
-
 import { Role } from "@prisma/client";
 import { ContactInformation, creerContact, modifierContact } from "../api/contact/contact";
 
 function transformerFormDataContact(FormData: FormData): ContactInformation {
+  const listeIdsRaw = FormData.getAll("listeIds");
+  const listeIds = listeIdsRaw
+    .map((v) => parseInt(v.toString(), 10))
+    .filter((n) => !isNaN(n));
   return {
     nom: FormData.get("nom")?.toString() ?? "",
     prenom: FormData.get("prenom")?.toString() ?? "",
     tel: FormData.get("tel")?.toString() ?? null,
     email: FormData.get("email")?.toString() ?? null,
     role: (FormData.get("role")?.toString() as Role) ?? null,
+    notes: FormData.get("notes")?.toString() ?? null,
+     listeIds,
   };
 }
 
