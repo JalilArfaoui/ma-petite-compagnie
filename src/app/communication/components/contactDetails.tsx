17fca640e72862bb827543747a726533c6396c55
Yuksel <ciranyuksel@gmail.com>
2026-04-07 11:46:45 +0200

    A1

--- src/app/communication/components/contactDetails.tsx
+++ src/app/communication/components/contactDetails.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Contact, Role } from "@prisma/client";
 import { useState } from "react";
-import { Box, Card, Input, Radio, RadioGroup, Stack, Button } from "@/components/ui";
+import { Box, Card, Input, Radio, RadioGroup, Stack, Button, Textarea } from "@/components/ui";
 
 export function ContactDetails({
   onSubmitted,
@@ -10,36 +10,41 @@ export function ContactDetails({
   onSubmitted: (donneeFormulaire: FormData) => void;
   contactDonnee: Contact | null;
 }) {
-  const [nom, setNom] = useState(contactDonnee?.nom ?? "");
+const [nom, setNom] = useState(contactDonnee?.nom ?? "");
   const [prenom, setPrenom] = useState(contactDonnee?.prenom ?? "");
   const [email, setEmail] = useState(contactDonnee?.email ?? "");
   const [tel, setTel] = useState(contactDonnee?.tel ?? "");
   const [role, setRole] = useState<Role>(contactDonnee?.role ?? "COMEDIEN");
+  const [ville, setVille] = useState(contactDonnee?.ville ?? "");
+  const [adresse, setAdresse] = useState(contactDonnee?.lieu ?? "");
+  const [notes, setNotes] = useState(contactDonnee?.notes ?? "");
   return (
-    <Card>
+   <Card>
       <form action={onSubmitted}>
         <Stack>
-          <Box>
-            Nom :{" "}
-            <Input
-              placeholder="Richard"
-              name="nom"
-              onChange={(e) => setNom(e.target.value)}
-              value={nom}
-              required
-            />
-          </Box>
+          <Stack className="w-full h-full" direction="row" justify="evenly">
+            <Box>
+              Nom :{" "}
+              <Input
+                placeholder="Richard"
+                name="nom"
+                onChange={(e) => setNom(e.target.value)}
+                value={nom}
+                required
+              />
+            </Box>
 
-          <Box>
-            Prenom :
-            <Input
-              name="prenom"
-              placeholder="Benoit"
-              onChange={(e) => setPrenom(e.target.value)}
-              value={prenom}
-              required
-            />
-          </Box>
+            <Box>
+              Prenom :
+              <Input
+                name="prenom"
+                placeholder="Benoit"
+                onChange={(e) => setPrenom(e.target.value)}
+                value={prenom}
+                required
+              />
+            </Box>
+          </Stack>
           <Box>
             Email :
             <Input
@@ -57,6 +62,33 @@ export function ContactDetails({
               placeholder="01234567891"
               onChange={(e) => setTel(e.target.value)}
               value={tel}
+            />
+          </Box>
+          <Box>
+            Ville
+            <Input
+              name="ville"
+              placeholder="Paris"
+              onChange={(e) => setVille(e.target.value)}
+              value={ville}
+            />
+          </Box>
+          <Box>
+            Adresse
+            <Input
+              name="adresse"
+              placeholder="Rue du Berger, 12000"
+              onChange={(e) => setAdresse(e.target.value)}
+              value={adresse}
+            />
+          </Box>
+          <Box>
+            Notes
+            <Textarea
+              name="notes"
+              placeholder="A contacter le 10 Mars"
+              onChange={(e) => setNotes(e.target.value)}
+              value={notes}
             />
           </Box>
           <Box>
