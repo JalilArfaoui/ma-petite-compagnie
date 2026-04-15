138256493e17229b7a35e4af9d81442cc8c90a0a
enzo2900 <133020135+enzo2900@users.noreply.github.com>
2026-04-15 10:23:38 +0200

    Modification si la liste existe

--- src/app/communication/api/contact/liste.ts
+++ src/app/communication/api/contact/liste.ts
@@ -1,20 +1,44 @@
 "use server";
 import { prisma } from "@/lib/prisma";
-import { Contact, ListeContact } from "@prisma/client";
+import { Contact } from "@prisma/client";
 import { resultOf } from "../../utils/helper";
 
+export async function getOneByNom(nomListe: string) {
+  try {
+    const resultat = await prisma.listeContact.findFirst({ where: { nom: nomListe } });
+    return resultOf(true, "", resultat);
+  } catch (error: unknown) {
+    console.log(error);
+    return resultOf(false, "Impossible de récupérer les listes par nom", null);
+  }
+}
 export async function createListe(nomListe: string, contacts: Contact[]) {
   try {
-    const resultat = prisma.listeContact.create({
-      data: {
-        nom: nomListe,
-        contacts: {
-          connect: contacts.map((contact) => {
-            return { id: contact.id };
-          }),
+    let resultat = null;
+    const liste = await getOneByNom(nomListe);
+    if (liste.succes && liste.donnee !== null) {
+      resultat = prisma.listeContact.update({
+        where: { id: liste.donnee.id },
+        data: {
+          contacts: {
+            connect: contacts.map((contact) => {
+              return { id: contact.id };
+            }),
+          },
         },
-      },
-    });
+      });
+    } else {
+      resultat = prisma.listeContact.create({
+        data: {
+          nom: nomListe,
+          contacts: {
+            connect: contacts.map((contact) => {
+              return { id: contact.id };
+            }),
+          },
+        },
+      });
+    }
     return resultOf(true, "", resultat);
   } catch (error: unknown) {
     console.log(error);
