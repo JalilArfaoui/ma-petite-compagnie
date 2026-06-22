ef64d49d6fefd08c6f444f63665ab3548390a6b4
sbonduel <124131393+sbonduel@users.noreply.github.com>
2026-06-23 01:38:07 +0200

    CI / Test (pull_request)

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import dotenv from "dotenv";
+
 dotenv.config();
 export default defineConfig({
   plugins: [react()],
@@ -17,6 +18,8 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
+
+      "next/dist/server"
+    }
   },
 });
